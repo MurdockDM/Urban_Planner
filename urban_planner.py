d777f78c28707ea0e118d38f9e2322106fe99d64
Dustin Murdock <dmurdock2@gmail.com>
2020-04-15 22:08:57 -0500

    initial commit

---
+++ urban_planner.py
@@ -0,0 +1,51 @@
+import datetime
+
+class Building:
+    """Create a building
+    Building(address, stories)
+
+    Keyword arguments :
+    address: string address of building
+    stories: number of stories
+    
+    """
+    def __init__(self, address, stories):
+        self.designer = ""
+        self.date_constructed = ""
+        self.owner = ""
+        self.address = address
+        self.stories = stories
+
+    def construct(self):
+        self.date_constructed = datetime.datetime.now()
+
+    def purchase(self, buyer):
+        self.owner = buyer
+
+    def printOut(self):
+        print(f"{self.address} was purchased by {self.owner} on {self.date_constructed} and has {self.stories} stories.")    
+
+fiveOhFive = Building("505 West Avenue", 3)
+fiveOhFive.purchase("Donald Donaldson")
+fiveOhFive.construct()
+fiveOhFive.printOut()
+
+fourOhFour = Building("404 East Avenue", 14)
+fourOhFour.purchase("Tom Thompson")
+fourOhFour.construct()
+fourOhFour.printOut()
+
+threeOhThree = Building("303 North Avenue", 9)
+threeOhThree.purchase("Bill Billson")
+threeOhThree.construct()
+threeOhThree.printOut()
+
+twoOhTwo = Building("202 South Avenue", 18)
+twoOhTwo.purchase("Chris Christenson")
+twoOhTwo.construct()
+twoOhTwo.printOut()
+
+oneOhOne = Building("101 West Boulevard", 7)
+oneOhOne.purchase("Robert Robertson")
+oneOhOne.construct()
+oneOhOne.printOut()
